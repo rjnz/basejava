e194a505cb37ce5b84763fa570960bd896b5b0e7
Andrew Semenikhin <semenikhin.andrew@gmail.com>
2022-05-16 22:35:01 +0300

    Implement clear

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,6 +5,11 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int counter = 0;
+        while (storage[counter] != null) {
+            storage[counter] = null;
+            storage[counter] = storage[counter++];
+        }
     }
 
     void save(Resume r) {
