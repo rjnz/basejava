a76fe4152f58e4c9897529602e1e9ddfad7c3a11
Andrew Semenikhin <semenikhin.andrew@gmail.com>
2022-05-16 23:24:08 +0300

    Implement getAll

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -49,7 +49,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(this.storage, this.size());
     }
 
     int size() {
